51f1dd764f9338c945307d6e19eaef7040743309
Alwin Emmanual <alwinblackace@gmail.com>
2022-11-15 01:14:19 +0530

    Initial setup

---
+++ react_msal/src/App.js
@@ -0,0 +1,74 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { config } from "./config/Config";
+import { PublicClientApplication } from "@azure/msal-browser";
+// import { MsalProvider } from "@azure/msal-react";
+import react, { useState } from "react";
+import styled from "styled-components";
+
+function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [user, setUser] = useState({});
+  const publicClientApp = new PublicClientApplication({
+    auth: {
+      clientId: config.appId,
+      redirectUri: config.redirectUri,
+      authotity: config.authority,
+    },
+    cache: {
+      cacheLocation: "sessionStorage", // This configures where your cache will be stored
+      storeAuthStateInCookie: true, // Set this to "true" if you are having issues on IE11 or Edge
+    },
+  });
+
+  async function logout() {
+    await publicClientApp.logoutRedirect();
+  }
+
+  async function login() {
+    try {
+      var auth = await publicClientApp.loginPopup({
+        scopes: config.scopes,
+        prompt: "select_account",
+      });
+      console.log(auth);
+      sessionStorage.setItem("accessToken_token", auth.accessToken);
+      setIsAuthenticated(true);
+    } catch (err) {
+      console.log(err);
+      setIsAuthenticated(false);
+      setUser({});
+    }
+  }
+
+  const Button = styled.button`
+    background-color: white;
+    color: black;
+    font-size: 20px;
+    padding: 10px 60px;
+    border-radius: 5px;
+    margin: 10px 0px;
+    cursor: pointer;
+    &:disabled {
+      color: grey;
+      opacity: 0.7;
+      cursor: default;
+    }
+  `;
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+          {!isAuthenticated ? (
+            <Button onClick={() => login()}>Login</Button>
+          ) : (
+            <Button onClick={() => logout()}>Logout</Button>
+          )}
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ react_msal/src/config/Config.js
@@ -0,0 +1,6 @@
+export const config = {
+  appId: "560e812a-2291-4a1f-8438-9ef0f97dffc0",
+  redirectUri: "http://localhost:3000/",
+  scopes: ["user.read"],
+  authority: "https://login.onmicrosoftonline.com",
+};
