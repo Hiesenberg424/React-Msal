95489a7f3988d5a94a0be14ba5ec09622e871e76
Alwin Emmanual <60211954+Hiesenberg424@users.noreply.github.com>
2022-11-15 16:36:29 +0530

    Update Config.js

--- react_msal/src/config/Config.js
+++ react_msal/src/config/Config.js
@@ -1,6 +1,6 @@
 export const config = {
   appId: "560e812a-2291-4a1f-8438-9ef0f97dffc0",
-  redirectUri: "https://6372acbb5309e00ede796fe9--celebrated-madeleine-95fd6d.netlify.app/",
+  redirectUri: "https://main--celebrated-madeleine-95fd6d.netlify.app/",
   scopes: ["user.read"],
   authority: "https://login.onmicrosoftonline.com",
 };
